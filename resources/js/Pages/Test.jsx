26ed1f1606227e4f00563a12e2fe45cb83cdf1f4
ishmael cascabel <cascabel_ishmael@yahoo.com>
2023-07-18 00:16:12 +0800

    setup

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test({user}) {
+    return (
+        <div className=''>Test Component Test {user}</div>
+    )
+}
+
+export default Test
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./resources/js/**/*.{js,jsx,tsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
